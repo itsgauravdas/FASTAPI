d8a7b77691edfedeb8026afc75a8f63a95f89b7a
PRACTICE_1 <dasgauravv@gmail.com>
2024-11-19 21:30:42 +0530

    adding

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-sqadd
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/") #[setting the path '/' = home ]
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/post") #[setting the path '/' = home ]
+def read_root():
+    return {"Hello": "This is post"}
+"""
+51.38 mins
+To run the server -->
+Uvicorn main(filename):app(app = FastAPI())
+Uvicorn main(filename):app(app = FastAPI()) --reload #to reloadthe server in every refresh afetr changes
+"""
